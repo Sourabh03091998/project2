6c3e99498f2196bb963afb5b1e285b8bd353da77
Sourabh Kakade <sourabhkakade123@gmail.com>
2023-08-16 22:03:00 +0530

    project 2 commit

---
+++ src/firsttest.java
@@ -0,0 +1,14 @@
+public class firsttest {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome Sourabh");
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ src/p.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class p {
+
+    public static void main(String[] args) {
+                String name = "Vishal";
+                String redColor = "\u001B[31m";
+                String resetColor = "\u001B[0m";
+                System.out.println(redColor + name + resetColor);
+            }
+        }
+
+
+
+
+
